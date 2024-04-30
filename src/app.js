713fea58366d5fff1b2ba5332f629d064d4e0437
Yann Klein <yann.klein@me.com>
2024-04-30 17:55:08 +0900

    livecode done

--- src/app.js
+++ src/app.js
@@ -1,85 +1,70 @@
 // DON'T CHANGE THIS LINE
-window.myBadAssGarage = "dwayne-wayne";
-if (myBadAssGarage) document.querySelector("#garage-name").innerText = myBadAssGarage.replace(/-/g, " ");
+window.myBadAssGarage = 'drift-container';
+if (myBadAssGarage)
+  document.querySelector('#garage-name').innerText = myBadAssGarage.replace(
+    /-/g,
+    ' ',
+  );
 
 // //////////////////////
 // PSEUDOCODE
 // //////////////////////
+const url = 'https://wagon-garage-api.herokuapp.com/drift-container/cars';
 
 // Get all the cars
 // //////////////////////
-// Steps
-// 1. Get all the necessary HTML elements -> cars-list, 
-// 2.5 Fetch an API -> Get the data about cars. 
-// 3 Change the DOM -> Inserting cars cards into the cars-list. 
-
-const displayCars = (carsInfo) => {
-  const carsList = document.querySelector(".cars-list");
-  carsList.innerHTML = '';
-  carsInfo.forEach((carInfo) => {
-    carsList.insertAdjacentHTML(
-      'beforeend', 
-      `<div class="car">
+const getCars = () => {
+  // Select class cars-list
+  const cars = document.querySelector('.cars-list');
+  // fetch GET api returns an array
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // iterate over the array and for each car
+      cars.innerHTML = '';
+      data.forEach((car) => {
+        const carDesc = `
+      <div class="car">
         <div class="car-image">
-          <img src="http://loremflickr.com/280/280/${carInfo.brand},${carInfo.model}" />
+          <img src="http://loremflickr.com/280/280/${car.brand},${car.model}" />
         </div>
         <div class="car-info">
-          <h4>${carInfo.brand} ${carInfo.model}</h4>
-          <p><strong>Owner:</strong> ${carInfo.owner}</p>
-          <p><strong>Plate:</strong> ${carInfo.plate}</p>
+          <h4>${car.brand} ${car.model}</h4>
+          <p><strong>Owner:</strong> ${car.owner}</p>
+          <p><strong>Plate:</strong> ${car.plate}</p>
         </div>
-      </div>`
-    );
-  });
+      </div>`;
+        // Insert the car into the cars-list
+        cars.insertAdjacentHTML('afterbegin', carDesc);
+      });
+    });
 };
-
-const getCars = () => {
-  const url = `https://wagon-garage-api.herokuapp.com/${myBadAssGarage}/cars`;
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      displayCars(data);
-  });
-}
 
 getCars();
 
 // Add a new car
 // //////////////////////
-// 1. Get all the necessary HTML elements -> new-car and input: brand, model, plate, owner
-const newCarForm = document.querySelector("#new-car");
-const brandEl = document.querySelector("#brand");
-const modelEl = document.querySelector("#model");
-const ownerEl = document.querySelector("#owner");
-const plateEl = document.querySelector("#plate");
+const addCar = document.querySelector('#submit-btn');
 
-// 2. Get the event listener -> submit on new-car
-newCarForm.addEventListener("submit", (event) => {
-  console.log(event);
+// add event listener(submit)
+addCar.addEventListener("click", (event) => {
   event.preventDefault();
-  // 3. API (POST) the input (using .value)
-  const url = `https://wagon-garage-api.herokuapp.com/${myBadAssGarage}/cars`;
-
-  const options = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      brand: brandEl.value,
-      model: modelEl.value,
-      owner: ownerEl.value,
-      plate: plateEl.value
-    })
-  }
-
-  fetch(url, options)
-    .then(response => response.json())
-    .then((data) => {
+  // select the inputs from the user
+  const brand = document.querySelector('#brand').value;
+  const model = document.querySelector('#model').value;
+  const plate = document.querySelector('#plate').value;
+  const owner = document.querySelector('#owner').value;
+  const data = { brand, model, plate, owner };
+  // fetch post the user inputs
+  fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  })
+    .then((response) => response.json())
+    .then(data => {
       console.log(data);
-      // 4. Get all the cars (once again)
-    })
-  getCars();
-  console.log("hi")
-});
-
-
-
+      // refresh the cars-list
+      getCars();
+    });
+})
